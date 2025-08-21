666555a7e400546597af01813f4de68b8a557a19
SaraVSSILVA <saravitoriads@outlook.com>
2025-08-21 12:14:45 -0300

    fix: adiciona prefixo global /api para rotas do backend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   // --- Configuração do Swagger ---
   const config = new DocumentBuilder()
